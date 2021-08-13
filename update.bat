688f5f3faa89878510be24b9a95a6a658cc3a947
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-13 10:54:22 +0200

    Add update command

---
+++ update.bat
@@ -0,0 +1,14 @@
+@REM Update application
+call git pull
+
+@REM Install server
+call cd server
+call npm install
+
+@REM Install client
+call cd ../client
+call npm install
+
+@REM Restart BB-Util
+call pm2 delete index
+call set NODE_ENV=production&&pm2 start server/index.js
