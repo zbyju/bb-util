cc60796a74a23073656c6cd7a18794b4281e56df
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-13 17:38:06 +0200

    Fix utility cards being static, now using config

--- client/src/components/Utility.vue
+++ client/src/components/Utility.vue
@@ -1,58 +1,65 @@
 <template>
-  <div class="Utility">
+  <div class="Utility" v-if="config">
     <h1>Utility</h1>
-
     <vs-row align="center">
-        <a @click="openLink(`${config.startup.pathToProgramSettings}`)" target="_blank" class="mr-5">
-            <vs-card type="3">
-                <template #title>
-                    <h3>Nastavovací stránka</h3>
-                </template>
-                <template #text>
-                <p>
-                    Otevřít nastavovací stránku Babyboxu - BBNP
-                </p>
-                </template>
-            </vs-card>
-        </a>
-        <a href="http://10.1.1.5" target="_blank" class="mr-5">
-            <vs-card type="3">
-                <template #title>
-                    <h3>10.1.1.5</h3>
-                </template>
-                <template #text>
-                <p>
-                    Otevřít jednotku motorů 10.1.1.5
-                </p>
-                </template>
-            </vs-card>
-        </a>
-        <a href="http://10.1.1.6" target="_blank" class="mr-5">
-            <vs-card type="3">
-                <template #title>
-                    <h3>10.1.1.6</h3>
-                </template>
-                <template #text>
-                <p>
-                    Otevřít jednotku topení 10.1.1.6
-                </p>
-                </template>
-            </vs-card>
-        </a>
-        <a href="http://10.1.1.7" target="_blank" class="mr-5">
-            <vs-card type="3">
-                <template #title>
-                    <h3>10.1.1.7</h3>
-                </template>
-                <template #text>
-                <p>
-                    Otevřít kameru 10.1.1.7
-                </p>
-                </template>
-            </vs-card>
-        </a>
-    </vs-row>
+      <a
+        @click="openLink(`${config.startup.pathToProgramSettings}`)"
+        target="_blank"
+        class="mr-5"
+      >
+        <vs-card type="3">
+          <template #title>
+            <h3>Nastavovací stránka</h3>
+          </template>
+          <template #text>
+            <p>
+              Otevřít nastavovací stránku Babyboxu - BBNP
+            </p>
+          </template>
+        </vs-card>
+      </a>
 
+      <template v-if="config.other">
+        <a
+          :href="`http://${config.other.SDSMotory}`"
+          target="_blank"
+          class="mr-5"
+        >
+          <vs-card type="3">
+            <template #title>
+              <h3>{{ config.other.SDSMotory }}</h3>
+            </template>
+            <template #text>
+              <p>Otevřít jednotku motorů</p>
+            </template>
+          </vs-card>
+        </a>
+        <a
+          :href="`http://${config.other.SDSTopeni}`"
+          target="_blank"
+          class="mr-5"
+        >
+          <vs-card type="3">
+            <template #title>
+              <h3>{{ config.other.SDSTopeni }}</h3>
+            </template>
+            <template #text>
+              <p>Otevřít jednotku topení</p>
+            </template>
+          </vs-card>
+        </a>
+        <a :href="`http://${config.other.Camera}`" target="_blank" class="mr-5">
+          <vs-card type="3">
+            <template #title>
+              <h3>{{ config.other.Camera }}</h3>
+            </template>
+            <template #text>
+              <p>Otevřít kameru</p>
+            </template>
+          </vs-card>
+        </a>
+      </template>
+    </vs-row>
   </div>
 </template>
 
@@ -60,25 +67,21 @@
 export default {
   name: "Utility",
   data: () => {
-    return {}
+    return {};
   },
   computed: {
     config() {
-      if(this.$store.state.editedConfig == null) {
-        this.$store.commit("mergeEditedConfig")
-      }
-      return this.$store.state.editedConfig
-    },
+      return this.$store.state.config;
+    }
   },
   methods: {
-      openLink(url) {
-        this.$store.dispatch("openLink", {
-            link: url
-        }) 
-      }
+    openLink(url) {
+      this.$store.dispatch("openLink", {
+        link: url
+      });
+    }
   }
 };
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
