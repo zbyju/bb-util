c684308e4ca2550a5f0e0852a932ae01f78f19da
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-13 19:11:54 +0200

    Add default firefox path button

--- client/src/components/Startup.vue
+++ client/src/components/Startup.vue
@@ -4,19 +4,38 @@
 
     <vs-row align="center" class="mt-5">
       <vs-col w="6">
-        <vs-input primary label="Cesta k programu (jen do kořene složky - např. C:\BB\)" placeholder="Výchozí: C:\BB\" v-model="config.pathToProgramFolder" :key="programKey" />
+        <vs-input
+          primary
+          label="Cesta k programu (jen do kořene složky - např. C:\BB\)"
+          placeholder="Výchozí: C:\BB\"
+          v-model="config.pathToProgramFolder"
+          :key="programKey"
+        />
       </vs-col>
       <vs-col w="6" class="pl-5">
-        <vs-button @click="insertDefaultProgramPath">Doplnit standardní cestu</vs-button>
+        <vs-button @click="insertDefaultProgramPath"
+          >Doplnit standardní cestu</vs-button
+        >
       </vs-col>
     </vs-row>
 
     <vs-row align="center" class="mt-5">
       <vs-col w="6">
-        <vs-input primary label="Cesta k prohlížeči" placeholder="Výchozí: C:\Program Files\Internet Explorer\iexplore.exe" v-model="config.pathToBrowser" :key="browserKey" />
+        <vs-input
+          primary
+          label="Cesta k prohlížeči"
+          placeholder="Výchozí: C:\Program Files\Internet Explorer\iexplore.exe"
+          v-model="config.pathToBrowser"
+          :key="browserKey"
+        />
       </vs-col>
       <vs-col w="6" class="pl-5">
-        <vs-button @click="insertDefaultBrowserPath">Doplnit standardní cestu</vs-button>
+        <vs-row>
+          <vs-button @click="insertDefaultIEPath">Doplnit cestu IE</vs-button>
+          <vs-button @click="insertDefaultFirefoxPath"
+            >Doplnit cestu Firefox</vs-button
+          >
+        </vs-row>
       </vs-col>
     </vs-row>
   </div>
@@ -28,25 +47,31 @@ export default {
   data: () => {
     return {
       programKey: 0, //To force update the input text
-      browserKey: 0  //To force update the input text
-    }
+      browserKey: 0 //To force update the input text
+    };
   },
   computed: {
     config() {
-      if(this.$store.state.editedConfig == null) {
-        this.$store.commit("mergeEditedConfig")
+      if (this.$store.state.editedConfig == null) {
+        this.$store.commit("mergeEditedConfig");
       }
-      return this.$store.state.editedConfig.startup
-    },
+      return this.$store.state.editedConfig.startup;
+    }
   },
   methods: {
     insertDefaultProgramPath() {
-      this.config.pathToProgramFolder = "C:\\BB\\"
-      ++this.programKey
+      this.config.pathToProgramFolder = "C:\\BB\\";
+      ++this.programKey;
     },
-    insertDefaultBrowserPath() {
-      this.config.pathToBrowser = "C:\\Program Files\\Internet Explorer\\iexplore.exe"
-      ++this.browserKey
+    insertDefaultIEPath() {
+      this.config.pathToBrowser =
+        "C:\\Program Files\\Internet Explorer\\iexplore.exe";
+      ++this.browserKey;
+    },
+    insertDefaultFirefoxPath() {
+      this.config.pathToBrowser =
+        "C:\\Program Files\\Mozilla Firefox\\firefox.exe";
+      ++this.browserKey;
     }
   }
 };
