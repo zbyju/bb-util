e3b2d3c9b21ec358e5f0e932b937ab3613bf99f1
Zbynek Jurica <zbynek.jurica@gmail.com>
2021-08-13 17:36:52 +0200

    Fix config not updating when it saving form

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -4,13 +4,9 @@
     <template v-if="configLoaded">
       <Startup />
       <Restart />
+      <OtherSettings />
 
-      <vs-button
-        success
-        gradient
-        class="submit"
-        @click="submit"
-      >
+      <vs-button success gradient class="submit" @click="submit">
         Uložit
       </vs-button>
     </template>
@@ -20,6 +16,7 @@
 <script>
 import Startup from "./Startup";
 import Restart from "./Restart";
+import OtherSettings from "./OtherSettings";
 import _ from "lodash";
 
 export default {
@@ -28,38 +25,58 @@ export default {
     return {
       notification: null,
       changeMade: false
-    }
+    };
   },
   computed: {
     config() {
-      if(this.$store.state.editedConfig == null) {
-        this.$store.commit("mergeEditedConfig")
+      if (this.$store.state.editedConfig == null) {
+        this.$store.commit("mergeEditedConfig");
       }
-      return this.$store.state.editedConfig
+      return this.$store.state.editedConfig;
     },
     configLoaded() {
-      return this.$store.state.configLoaded
+      return this.$store.state.configLoaded;
     },
     valid() {
-      return this.$store.state.valid
+      return this.$store.state.valid;
     }
   },
   components: {
     Startup,
-    Restart
+    Restart,
+    OtherSettings
   },
   methods: {
     async submit() {
-      if(this.formValid()) {
+      if (this.formValid()) {
         try {
-          await this.$store.dispatch("putConfig")
-          this.openNotification(5000, "top-right", "success", "Data úspěšně uložena", "Data, která jste zadali do formuláře, byla úspěšně uložena.")
-        } catch(err) {
-          console.log(err)
-          this.openNotification(5000, "top-right", "danger", "Data se nepodařilo uložit.", "Data, která jste zadali do formuláře, nebyla úspěšně uložena.")
+          await this.$store.dispatch("putConfig");
+          await this.$store.dispatch("getConfig");
+          this.openNotification(
+            5000,
+            "top-right",
+            "success",
+            "Data úspěšně uložena",
+            "Data, která jste zadali do formuláře, byla úspěšně uložena."
+          );
+        } catch (err) {
+          console.log(err);
+          this.openNotification(
+            5000,
+            "top-right",
+            "danger",
+            "Data se nepodařilo uložit.",
+            "Data, která jste zadali do formuláře, nebyla úspěšně uložena."
+          );
         }
       } else {
-        this.openNotification(5000, "top-right", "danger", "Chyba dat", "Ve formulářových datech je chyba, proto je nelze uložit.")
+        this.openNotification(
+          5000,
+          "top-right",
+          "danger",
+          "Chyba dat",
+          "Ve formulářových datech je chyba, proto je nelze uložit."
+        );
       }
     },
     openNotification(duration, position = null, color, title, text) {
@@ -69,34 +86,43 @@ export default {
         position,
         title,
         text
-      })
+      });
     },
     closeNotification() {
-      this.notification.close()
+      this.notification.close();
     },
     formValid() {
-      let valid = true
-      for(let key in this.valid.restart) {
-        if(!this.valid.restart[key]) {
-          valid = false
+      let valid = true;
+      for (let key in this.valid.restart) {
+        if (!this.valid.restart[key]) {
+          valid = false;
         }
       }
-      return valid
-    },
+      return valid;
+    }
   },
   watch: {
     config: {
       deep: true,
       handler: function() {
-        this.changeMade = !_.isEqual(this.$store.state.editedConfig, this.$store.state.config)
+        this.changeMade = !_.isEqual(
+          this.$store.state.editedConfig,
+          this.$store.state.config
+        );
       }
     },
     changeMade(newValue) {
-      if(newValue == true) {
-        this.notification = this.openNotification("none", "bottom-center", "dark", "Neuložené změny", "Provedli jste změny, které nebyly uloženy! Pro uložení stiskněte tlačítko pod formulářem.")
+      if (newValue == true) {
+        this.notification = this.openNotification(
+          "none",
+          "bottom-center",
+          "dark",
+          "Neuložené změny",
+          "Provedli jste změny, které nebyly uloženy! Pro uložení stiskněte tlačítko pod formulářem."
+        );
       } else {
-        if(this.notification) {
-          this.notification.close()
+        if (this.notification) {
+          this.notification.close();
         }
       }
     }
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from 'axios'
+import axios from "axios";
 
 import _ from "lodash";
 
@@ -16,6 +16,11 @@ export default new Vuex.Store({
         maxTimeDifference: true,
         delayToNextRestart: true,
         errorsInRowToRestart: true
+      },
+      other: {
+        ipMotory: true,
+        ipTopeni: true,
+        ipCamera: true
       }
     },
     data: []
@@ -25,20 +30,23 @@ export default new Vuex.Store({
       state.editedConfig = _.merge({}, state.config);
     },
     updateConfig(state, config) {
-      state.config = _.merge(config, state.config)
+      state.config = _.merge(state.config, config);
     },
     updateEditedConfig(state, config) {
-      state.editedConfig = _.merge(state.editedConfig, config)
+      state.editedConfig = _.merge(state.editedConfig, config);
     },
     addData(state, newData) {
-      if(!newData || !newData.babyboxData || !newData.babyboxData.id) {
-        return
+      if (!newData || !newData.babyboxData || !newData.babyboxData.id) {
+        return;
       }
-      if(state.data.length == 0 || newData.babyboxData.id != state.data[0].babyboxData.id) {
-        if(state.data.length < 30) {
+      if (
+        state.data.length == 0 ||
+        newData.babyboxData.id != state.data[0].babyboxData.id
+      ) {
+        if (state.data.length < 30) {
           state.data.unshift(newData);
         } else {
-          state.data.pop()
+          state.data.pop();
           state.data.unshift(newData);
         }
       }
@@ -49,19 +57,21 @@ export default new Vuex.Store({
       return new Promise((resolve, reject) => {
         axios({
           method: "GET",
-          url: "/config/",
+          url: "/config/"
         })
-        .then(response => response.data)
-        .then(responseConfig => {
-          context.commit("updateConfig", responseConfig)
-          context.commit("updateEditedConfig", responseConfig)
-          context.state.configLoaded = true
-          resolve(responseConfig)
-        })
-        .catch(err => {
-          reject(err)
-        })
-      })
+          .then(response => {
+            return response.data;
+          })
+          .then(responseConfig => {
+            context.commit("updateConfig", responseConfig);
+            context.commit("updateEditedConfig", responseConfig);
+            context.state.configLoaded = true;
+            resolve(responseConfig);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
     },
     async openLink(context, link) {
       return new Promise(() => {
@@ -71,8 +81,8 @@ export default new Vuex.Store({
           data: {
             link
           }
-        })
-      })
+        });
+      });
     },
     async putConfig(context) {
       return new Promise((resolve, reject) => {
@@ -83,33 +93,32 @@ export default new Vuex.Store({
             config: context.state.editedConfig
           }
         })
-        .then(response => response.data)
-        .then(data => {
-          context.commit("updateConfig", data.config)
-          context.commit("updateEditedConfig", data.config)
-          resolve(data.config)
-        })
-        .catch(err => {
-          reject(err)
-        })
-      })
+          .then(response => response.data)
+          .then(async data => {
+            context.commit("updateConfig", data.config);
+            context.commit("updateEditedConfig", data.config);
+            resolve(data.config);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
     },
     async getData(context) {
       return new Promise((resolve, reject) => {
         axios({
           method: "GET",
-          url: "/data/",
+          url: "/data/"
         })
-        .then(response => response.data)
-        .then(data => {
-          context.commit("addData", data)
-          resolve(data)
-        })
-        .catch(err => {
-          reject(err)
-        })
-      })
-    },
-  },
+          .then(response => response.data)
+          .then(data => {
+            context.commit("addData", data);
+            resolve(data);
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    }
+  }
 });
-
